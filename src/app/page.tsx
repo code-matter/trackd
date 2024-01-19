8ede7376508fae744e19816282f1c22bd9a654a8
Alexandre <alexandre.caissy@norda.com>
2024-01-19 16:23:27 -0500

    feat: deploy test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { getBudgetEntries } from "./services/BudgetEntries";
 
 export default async function Home() {
   const budgetEntries = await getBudgetEntries();
-  console.log("budgetEntries", budgetEntries);
+
   return (
     <main className="main">
       <h1>Entries</h1>
