1e5974804b010a630671ca0035e2f6b7f98fa386
Alexandre <alexandre.caissy@norda.com>
2024-01-19 15:32:52 -0500

    feat: added prisma + postgres + vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-"use client";
+// import { sql } from "@vercel/postgres";
 import { Button } from "antd";
 
 export default function Home() {
+  // const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
   return (
     <main className="main">
       <h1>hello</h1>
