1071d69a988826acd8efdf92144af3745a14e10f
Alexandre <alexandre.caissy@norda.com>
2024-01-19 14:44:03 -0500

    set up

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <main className="main">
+      <h1>hello</h1>
+      <Button>OK</Button>
+    </main>
+  );
+}
