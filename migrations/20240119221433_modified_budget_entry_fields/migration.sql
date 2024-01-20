5757787c51517516e9021408daeb1dfdf1e3a22e
Alexandre <alexandre.caissy@norda.com>
2024-01-20 17:02:08 -0500

    fix: resetting DB

--- migrations/20240119221433_modified_budget_entry_fields/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "budget_entries" ALTER COLUMN "created_at" DROP NOT NULL,
-ALTER COLUMN "updated_at" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "users" ALTER COLUMN "created_at" DROP NOT NULL,
-ALTER COLUMN "updated_at" DROP NOT NULL,
-ALTER COLUMN "updated_at" SET DEFAULT CURRENT_TIMESTAMP;
